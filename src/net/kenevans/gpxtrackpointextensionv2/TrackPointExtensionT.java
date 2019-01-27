5bc0ede67540fefba26e782c25687654db01b963
Kenneth Evans <ken@kenevans.net>
2019-01-26 23:21:42 -0500

    TrackPointExtensionT has @XmlRootElement.  Copied new trackpointextensionv2.GPXParser from net.kenevans.gpx.

--- src/net/kenevans/gpxtrackpointextensionv2/TrackPointExtensionT.java
+++ src/net/kenevans/gpxtrackpointextensionv2/TrackPointExtensionT.java
@@ -12,6 +12,7 @@ import java.math.BigDecimal;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlSchemaType;
 import javax.xml.bind.annotation.XmlType;
 
@@ -49,6 +50,7 @@ import javax.xml.bind.annotation.XmlType;
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement
 @XmlType(name = "TrackPointExtension_t", namespace = "http://www.garmin.com/xmlschemas/TrackPointExtension/v2", propOrder = {
     "atemp",
     "wtemp",
